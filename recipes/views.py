9cde8de032c76f2451281bc67324e59d7dddefe5
Henrique Stocco <henriquestoccomachado@gmail.com>
2023-11-05 11:01:36 -0300

    created name spacing with templates and base

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 
 def about(request):
