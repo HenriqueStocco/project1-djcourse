766847509a49fc095ea53ae035fa32d8eaf33ce0
Henrique Stocco <henriquestoccomachado@gmail.com>
2023-11-16 06:44:20 -0300

    created models and make migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    # DateTimeField > auto_now_add > gera automaticamente a data inicial de criaçao da recipe
+    created_at = models.DateTimeField(auto_now_add=True)
+    # DateTimeField > auto_now > gera a data de update da recipe
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    # Faz relaçao com a tabela Category, e se nao existir a categoria ou for excluida, ele setta esse campo como null, null=True significa que esse campo pode ser null, esta habilitando
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    # Faz relaçao com a tabela User, onde vai ser usado a propria tabela dele para salvar os author, seguindo a mesma logica de category, se nao exister ou for excluido, o campo se torna null
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
