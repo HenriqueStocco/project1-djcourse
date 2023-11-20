d36b3d1447ffbe36eb63e220c4a3268615a89310
Henrique Stocco <henriquestoccomachado@gmail.com>
2023-11-20 10:02:10 -0300

    created models, added in admin for editing and creating new recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
 # Register your models here.
+# @admin.register e o mesmo que admin.site.register(model, adminclass)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -18,14 +21,24 @@ class Recipe(models.Model):
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
-    # DateTimeField > auto_now_add > gera automaticamente a data inicial de criaçao da recipe
+    '''DateTimeField > auto_now_add > gera automaticamente a data inicial de
+    criaçao da recipe'''
     created_at = models.DateTimeField(auto_now_add=True)
     # DateTimeField > auto_now > gera a data de update da recipe
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    # Faz relaçao com a tabela Category, e se nao existir a categoria ou for excluida, ele setta esse campo como null, null=True significa que esse campo pode ser null, esta habilitando
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    '''Faz relaçao com a tabela Category, e se nao existir a categoria ou for
+    excluida, ele setta esse campo como null, null=True significa que esse
+    campo pode ser null, esta habilitando
+    blank permite deixar sem imagem na recipe e default diz que por padrao ela nao recebe imagem'''
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
-    # Faz relaçao com a tabela User, onde vai ser usado a propria tabela dele para salvar os author, seguindo a mesma logica de category, se nao exister ou for excluido, o campo se torna null
+    ''' Faz relaçao com a tabela User, onde vai ser usado a propria tabela dele
+    para salvar os author, seguindo a mesma logica de category, se nao exister
+    ou for excluido, o campo se torna null '''
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
