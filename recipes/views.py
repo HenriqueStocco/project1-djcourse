8ad5015bc41d36ec27035c835b1323bbefcf1fff
Henrique Stocco <henriquestoccomachado@gmail.com>
2023-11-21 06:47:05 -0300

    added category page, id for each page and whitespace enable in category

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,7 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -18,9 +18,9 @@ def category(request, category_id):
     fica category__id(um campo de Recipe que esta relacionado com Category)=category_id(que e o nome que a url informa)
     '''
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id, is_published=True
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
